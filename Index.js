2e971748e542a319ebe014868efd25f131ebffd6
Jusno <Jusno@foxmail.com>
2018-11-16 22:52:00 +0800

    加入日志

--- Index.js
+++ Index.js
@@ -1,10 +1,29 @@
 const http =require('http');
+const fs=require('fs');
+const path=require('path');
 
 let server=http.createServer();
 console.log('server-----start');
 server.on('request',(reqMsg,rsp)=>{
     let url=reqMsg.url;
     if(url.indexOf('/favicon.ico') != -1) return ;
+    //日志
+    let nowDate=new Date();
+    let fileName=nowDate.getFullYear()+'_'+(nowDate.getMonth()+1)+'_'+nowDate.getDate()+'.txt';
+    let filePath=path.join(__dirname,'./log/')
+    if(!fs.existsSync(filePath)){
+        fs.mkdirSync(filePath);
+    }
+    filePath+=fileName;
+    let data=reqMsg.connection.remoteAddress+'\n';
+    
+    fs.appendFile(filePath,data,'utf8',function(err){
+        if(err)
+        {
+            console.log(err);
+        }
+    });
+
     rsp.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
 
     if(url.indexOf('/serverCtrl=close') != -1){
